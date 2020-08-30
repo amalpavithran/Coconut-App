eff83ea2d7f207ed6d3ae6711c863af3950c8f31
Amal Pavithran <amalpavithranmp@gmail.com>
2020-08-30 12:57:30 +0530

    Just some clean up

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:coconut_app/auth_repo.dart';
-import 'package:coconut_app/cubit/cubit/login_cubit.dart';
+import 'package:coconut_app/presentation/cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
