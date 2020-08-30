11d28afdf613d591fc5de133ffd74f6f5c02c637
hemanthkumar17 <hemanth17112000@gmail.com>
2020-08-30 12:38:07 +0530

    Updated Payment Repo

--- lib/payment_repo.dart
+++ lib/payment_repo.dart
@@ -2,18 +2,16 @@ import 'package:coconut_app/models/pay_details.dart';
 import 'package:upi_india/upi_india.dart';
 
 abstract class PaymentRepository {
-  Future<String> initiatePayment(
+  Future<Map<String, String>> initiatePayment(
       PaymentDetails details); // Returns a response, Success, Failure+error
 }
 
 class Payment implements PaymentRepository {
   @override
-  Future<String> initiatePayment(PaymentDetails details) {
+  // ignore: missing_return
+  Future<Map<String, String>> initiatePayment(PaymentDetails details) {
     UpiIndia _upiIndia = UpiIndia();
-
-    _upiIndia.getAllUpiApps().then((value) {
-      if (!value.contains(UpiApp.GooglePay)) return "Failure";
-    });
+    Map<String, String> response;
 
     _upiIndia
         .startTransaction(
@@ -22,9 +20,27 @@ class Payment implements PaymentRepository {
             receiverName: details.recieverName,
             transactionNote: details.transactionNote,
             amount: details.amount)
-        .then((value) {
-      print(value);
-      return value.toString();
+        .then((_upiResponse) {
+      print(_upiResponse);
+      if (_upiResponse.error != null) {
+        response["Status"] = "Failure";
+        switch (_upiResponse.error) {
+          case UpiError.APP_NOT_INSTALLED:
+            response["Report"] = "Requested app not installed on device";
+            break;
+          case UpiError.INVALID_PARAMETERS:
+            response["Report"] = "Requested app cannot handle the transaction";
+            break;
+          case UpiError.NULL_RESPONSE:
+            response["Report"] = "Requested app didn't returned any response";
+            break;
+          case UpiError.USER_CANCELLED:
+            response["Report"] = "You cancelled the transaction";
+            break;
+        }
+      } else
+        response["Status"] = "Success";
+      return response;
     });
   }
 }
