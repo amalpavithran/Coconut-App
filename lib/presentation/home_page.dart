343f6869d0863ae2c8db34652806381d2767b2a8
Amal Pavithran <amalpavithranmp@gmail.com>
2020-08-30 17:42:27 +0530

    Home Page UI

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:coconut_app/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -47,20 +48,47 @@ class HomePage extends StatelessWidget {
     );
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My Account'),
-          toolbarHeight: 150,
-          actions: <Widget>[
+      appBar: AppBar(
+        title: Text('My Account'),
+        flexibleSpace: SizedBox(height: 200),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              iconSize: 35,
+              icon: Icon(Icons.account_circle, color: Colors.white),
+              onPressed: null,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                iconSize: 35,
-                icon: Icon(Icons.account_circle, color: Colors.white),
-                onPressed: null,
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                      color: Colors.green,
+                      child: Text("Create New Group",style: TextStyle(color:Colors.white),),
+                      onPressed: (){}),
+                  RaisedButton(
+                    color: Colors.blue[200],
+                    child: Text("Join a Group",style: TextStyle(color:Colors.white),),
+                    onPressed: (){
+
+                    },
+                  )
+                ],
               ),
             ),
+            groupList,
           ],
         ),
-        body: Padding(padding: EdgeInsets.only(top: 20), child: groupList));
+      ),
+    );
   }
 }
