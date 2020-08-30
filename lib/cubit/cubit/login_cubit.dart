91ee7e15075a4360d87b766bd6a6297561a13f77
Amal Pavithran <amalpavithranmp@gmail.com>
2020-08-30 12:44:47 +0530

    Login Page implemented

---
+++ lib/cubit/cubit/login_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:coconut_app/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository authRepository;
+  LoginCubit(this.authRepository) : super(LoginInitial());
+
+  void login(String phoneNumber, String password) async {
+    emit(LoginLoading());
+    try {
+      final result = await authRepository.login(phoneNumber, password);
+      emit(LoginSuccess());
+    } catch (e) {
+      print(e);
+      LoginFailure("Login Failure");
+    }
+  }
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,105 @@
+import 'package:coconut_app/auth_repo.dart';
+import 'package:coconut_app/cubit/cubit/login_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _phoneNumber;
+  String _password;
+  @override
+  Widget build(BuildContext context) {
+    final phoneNumber = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
+      child: TextFormField(
+        textCapitalization: TextCapitalization.characters,
+        validator: (value) {
+          if (value.length == 10) {
+            return null;
+          } else {
+            return "Enter a valid phone number";
+          }
+        },
+        onSaved: (String value) {
+          this._phoneNumber = value;
+        },
+        decoration: InputDecoration(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+            labelText: "Roll Number",
+            border: OutlineInputBorder()),
+      ),
+    );
+    final password = Padding(
+      padding: EdgeInsets.symmetric(vertical: 7.0, horizontal: 10.0),
+      child: TextFormField(
+        enabled: false,
+        onSaved: (value) {
+          this._password = value;
+        },
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+        decoration: InputDecoration(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+            labelText: "Password",
+            labelStyle: TextStyle(color: Colors.grey),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+    );
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => LoginCubit(AuthRepositoryImpl()),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              phoneNumber,
+              password,
+              BlocBuilder<LoginCubit, LoginState>(
+                builder: (context, state) {
+                  if (state is LoginLoading) {
+                    return Container(
+                      height: 50,
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                          BlocProvider.of<LoginCubit>(context)
+                              .login(_phoneNumber, _password);
+                        }
+                      },
+                      child: Text("Login"),
+                    );
+                  }
+                },
+              ),
+              BlocListener<LoginCubit, LoginState>(
+                listener: (BuildContext context, LoginState state) {
+                  if (state is LoginFailure) {
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text(state.message)));
+                  }else if(state is LoginSuccess){
+                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("Login Success")));
+                    //TODO: Implement Push to Home
+                  }
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
