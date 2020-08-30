8822222bbd6294212f1eaaf082ff189630f35de3
Amal Pavithran <amalpavithranmp@gmail.com>
2020-08-30 16:51:33 +0530

    Linking to homePage

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:coconut_app/presentation/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   runApp(MyApp());
 }
 
@@ -12,11 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: LoginPage());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+      routes: {
+        '\homepage': (context)=>HomePage(),
+      },
+    );
   }
 }
