92a80c05169a0cf9f13342f7fff02bb80bf3a0d6
hemanthkumar17 <hemanth17112000@gmail.com>
2020-08-30 16:45:44 +0530

    Added: skeletal scroll

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:coconut_app/models/user.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,25 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<UserDetails> users = [
+      UserDetails(
+          email: "hemanth@gmail.com",
+          name: "Hemanth",
+          photoURL: null,
+          groups: null)
+    ];
+    List<Widget> groupWids = [];
+    for (var user in users) {
+      groupWids.add(Card(
+        child: Text(user.email),
+      ));
+    }
+    Widget groupList = SingleChildScrollView(
+      child: Column(
+        children: groupWids,
+      ),
+    );
+
     return Scaffold(
         body: CustomScrollView(
       slivers: <Widget>[
